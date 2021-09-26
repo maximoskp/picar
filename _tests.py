e4409e82e293d4fde7c1d742ff9c3618c6cd53d8
maximoskalpap <maximos@athenarc.gr>
2021-09-26 09:01:03 +0300

    initial commit

---
+++ _tests.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Sep 26 08:59:57 2021
+
+@author: user
+"""
+
+# https://inputs.readthedocs.io/en/latest/user/quickstart.html
+
+from inputs import devices
+
+for device in devices:
+    print(device)
